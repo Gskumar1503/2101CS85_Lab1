54b3bfa5b6c9ac972e2ab12c7a9cb3b6f05203bf
Gonnabattula Sowjanya Kumar <110079940+Gskumar1503@users.noreply.github.com>
2022-08-24 19:47:30 +0530

    Bubble sort

--- sorting.c
+++ sorting.c
@@ -1,37 +1,30 @@
-// C program for implementation of selection sort
+// C program for implementation of Bubble sort
 #include <stdio.h>
 
-void swap(int *xp, int *yp)
+void swap(int* xp, int* yp)
 {
 	int temp = *xp;
 	*xp = *yp;
 	*yp = temp;
 }
 
-void selectionSort(int arr[], int n)
+// A function to implement bubble sort
+void bubbleSort(int arr[], int n)
 {
-	int i, j, min_idx;
+	int i, j;
+	for (i = 0; i < n - 1; i++)
 
-	// One by one move boundary of unsorted subarray
-	for (i = 0; i < n-1; i++)
-	{
-		// Find the minimum element in unsorted array
-		min_idx = i;
-		for (j = i+1; j < n; j++)
-		if (arr[j] < arr[min_idx])
-			min_idx = j;
-
-		// Swap the found minimum element with the first element
-		if(min_idx != i)
-			swap(&arr[min_idx], &arr[i]);
-	}
+		// Last i elements are already in place
+		for (j = 0; j < n - i - 1; j++)
+			if (arr[j] > arr[j + 1])
+				swap(&arr[j], &arr[j + 1]);
 }
 
 /* Function to print an array */
 void printArray(int arr[], int size)
 {
 	int i;
-	for (i=0; i < size; i++)
+	for (i = 0; i < size; i++)
 		printf("%d ", arr[i]);
 	printf("\n");
 }
@@ -39,9 +32,9 @@ void printArray(int arr[], int size)
 // Driver program to test above functions
 int main()
 {
-	int arr[] = {64, 25, 12, 22, 11};
-	int n = sizeof(arr)/sizeof(arr[0]);
-	selectionSort(arr, n);
+	int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	bubbleSort(arr, n);
 	printf("Sorted array: \n");
 	printArray(arr, n);
 	return 0;
