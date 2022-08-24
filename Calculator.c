300588562ff826bbcb9673003d8709a7b5311bc5
Gonnabattula Sowjanya Kumar <110079940+Gskumar1503@users.noreply.github.com>
2022-08-24 19:24:51 +0530

    Sub

--- Calculator.c
+++ Calculator.c
@@ -4,15 +4,15 @@ int main()
     double x,y;//Take a two numbers from user
     printf("Enter any two numbers:");
     scanf("%lf %lf",&x,&y);
-    //Take the operation between  above two numbers like +
-    printf("Take only 1 for + \n");
+    //Take the operation between  above two numbers like -
+    printf("Take only 2 for - \n");
     int z;
     printf("Enter the number for operation that you want the result=");
     scanf("%d",&z);
     switch(z)
     {
-        case 1:
-        printf("Sum of above two input numbers=%lf",x+y);
+        case 2:
+        printf("Difference  of above two input numbers=%lf",x-y);
     }
     return 0;
 }
