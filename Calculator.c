fb9f2c23dac213abf7431cfa140f15c4f651d238
Gonnabattula Sowjanya Kumar <110079940+Gskumar1503@users.noreply.github.com>
2022-08-24 19:27:37 +0530

    Div

--- Calculator.c
+++ Calculator.c
@@ -4,15 +4,15 @@ int main()
     double x,y;//Take a two numbers from user
     printf("Enter any two numbers:");
     scanf("%lf %lf",&x,&y);
-    //Take the operation between  above two numbers like *
-    printf("Take only 3 for * \n");
+    //Take the operation between  above two numbers like /
+    printf("Take only 4 for / \n");
     int z;
     printf("Enter the number for operation that you want the result=");
     scanf("%d",&z);
     switch(z)
     {
-        case 3:
-        printf("Product   of above two input numbers=%lf",x*y);
+        case 4:
+        printf("Quotient   of above two input numbers=%lf",x/y);
     }
     return 0;
 }
